ef9e0769f0352e91f27a09a3dd12be6647d7da22
oyeong011 <oyeong011@gmail.com>
2024-03-09 12:50:08 +0900

    [feat] WA-1 코드 변경

--- src/utils/clipboardUtils.jsx
+++ src/utils/clipboardUtils.jsx
@@ -5,6 +5,6 @@ export const handleCopyClipBoard = (text) => {
     navigator.clipboard.writeText(text);
     alert("클립보드에 복사되었습니다.");
   } catch (error) {
-    alert("클립보드 복사에 실패하였습니다.");
+    alert("클립보드 복사에 실패를한거같은데 맞는거였습니다.");
   }
 };
