b1bdbf66433ecf6030515438dfae0be7293e66d1
oyeong011 <oyeong011@gmail.com>
2024-03-09 14:41:20 +0900

    [feat] WA-1 라우팅 세팅

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.n6oabueagjg"
+    "revision": "0.k5ijdlb996o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
