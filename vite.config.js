f0a41f4199f0f5c808f30b1bc66f6f459a4a0b51
oyeong011 <oyeong011@gmail.com>
2024-03-11 19:32:02 +0900

    [feat] WA-1 pwa세팅

--- vite.config.js
+++ vite.config.js
@@ -7,12 +7,39 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
+      injectRegister: "auto",
       registerType: "autoUpdate",
       devOptions: {
         enabled: true,
       },
       workbox: {
         globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      manifest: {
+        theme_color: "#655D53",
+        background_color: "#EEE4D2",
+        icons: [
+          {
+            src: "/public/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/public/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "/public/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "/public/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
       },
     }),
   ],
